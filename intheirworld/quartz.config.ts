5014aa4399abad23f5df91b9763be65038e5c1ad
Miguel Sicart <deterbold@gmail.com>
2026-06-12 07:39:28 +0200

    Update baseUrl to subdomain

--- intheirworld/quartz.config.ts
+++ intheirworld/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ridiculous.software/intheirworld",
+    baseUrl: "intheirworld.ridiculous.software",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
